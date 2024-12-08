96e11852a9c83d2f905c7ede70659a343a8a99f4
ktsouvalis <67092537+ktsouvalis@users.noreply.github.com>
2024-12-08 18:47:04 +0200

    refactor StoreArticles to show the number of records ignored due to existing id inside the mappedData (sources may send the same article twice)

--- app/Actions/StoreArticles.php
+++ app/Actions/StoreArticles.php
@@ -12,11 +12,36 @@ class StoreArticles
 {
     use AsAction;
 
+    // public function handle(array $mappedData)
+    // {
+    //     foreach ($mappedData as $data) {
+    //         $articles = [];
+    //         $articles[] = [
+    //             'doc_id' => $data['doc_id'],
+    //             'source' => $data['source'],
+    //             'published_at' => $data['published_at'],
+    //             'author' => $data['author'] ?? null,
+    //             'category' => $data['category'],
+    //             'content' => json_encode($data['content']),
+    //             'created_at' => Carbon::now(),
+    //         ];
+    //         try {
+    //             Article::insertOrIgnore($articles);
+    //         } catch (Exception $e) {
+    //             Log::error($e->getMessage());
+    //             continue;
+    //         }
+    //     }
+    //     Log::info(Article::count() . ' articles stored successfully');
+    // }
+
     public function handle(array $mappedData)
     {
+        $totalRecords = count($mappedData);
+        $uniqueRecords = [];
+
         foreach ($mappedData as $data) {
-            $articles = [];
-            $articles[] = [
+            $uniqueRecords[$data['doc_id']] = [
                 'doc_id' => $data['doc_id'],
                 'source' => $data['source'],
                 'published_at' => $data['published_at'],
@@ -24,14 +49,21 @@ class StoreArticles
                 'category' => $data['category'],
                 'content' => json_encode($data['content']),
                 'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now(),
             ];
-            try {
-                Article::insertOrIgnore($articles);
-            } catch (Exception $e) {
-                Log::error($e->getMessage());
-                continue;
-            }
         }
+
+        $uniqueRecords = array_values($uniqueRecords);
+        $uniqueCount = count($uniqueRecords);
+
+        try {
+            Article::insertOrIgnore($uniqueRecords);
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+        }
+
+        $ignoredRecords = $totalRecords - $uniqueCount;
+        Log::info("Total records: $totalRecords, Unique records: $uniqueCount, Ignored records: $ignoredRecords");
     }
 
     public function asJob(array $mappedData)
