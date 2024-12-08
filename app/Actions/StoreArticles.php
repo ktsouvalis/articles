0384d0ca83af066d172f1e62219812112eec5cd6
ktsouvalis <67092537+ktsouvalis@users.noreply.github.com>
2024-12-08 14:02:25 +0200

    handles data storing seperately for each source, handles error to go to the next source's data

--- app/Actions/StoreArticles.php
+++ app/Actions/StoreArticles.php
@@ -2,9 +2,11 @@
 
 namespace App\Actions;
 
-use App\Models\Article;
-use Lorisleiva\Actions\Concerns\AsAction;
+use Exception;
 use Carbon\Carbon;
+use App\Models\Article;
+use Illuminate\Support\Facades\Log;
+use Lorisleiva\Actions\Concerns\AsAction;
 
 class StoreArticles
 {
@@ -13,8 +15,8 @@ class StoreArticles
     public function handle(array $mappedData)
     {
         foreach ($mappedData as $source) {
-            $articles = [];
             foreach($source as $data){
+                $articles = [];
                 $articles[] = [
                     'doc_id' => $data['doc_id'],
                     'source' => $data['source'],
@@ -24,8 +26,14 @@ class StoreArticles
                     'content' => json_encode($data['content']),
                     'created_at' => Carbon::now(),
                 ];
+                try{
+                    Article::insertOrIgnore($articles);
+                } 
+                catch (Exception $e){
+                    Log::error($e->getMessage());
+                    continue;
+                }
             }
-            Article::insertOrIgnore($articles);
         }
     }
 
