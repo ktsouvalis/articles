1e311dc56a4f06ea35f93862f2004c609ed61bd1
Konstantinos Tsouvalis <ktsouvalis@uop.gr>
2024-12-09 12:38:33 +0200

    refactor CallSources code

--- app/Actions/CallSources.php
+++ app/Actions/CallSources.php
@@ -18,7 +18,23 @@ class CallSources
 
     public function handle()
     {
-        $sources = [
+        $sources = $this->getSources();
+
+        foreach ($sources as $source) {
+            $flattened_data = $this->fetchAndMapData($source);
+            
+            if (empty($flattened_data)) {
+                Log::info('No new articles found in ' . $source['name']);
+                continue;
+            }
+
+            StoreArticles::dispatch($flattened_data, $source['name']);
+        }
+    }
+
+    private function getSources()
+    {
+        return [
             [
                 'name' => 'NYTimes',
                 'url' => env('NYTIMES_API_URL'),
@@ -49,39 +65,20 @@ class CallSources
                 'page_size' => 100,
                 'mapper' => new NewsAPIMapper()
             ],
-
             // Add more sources here
-            // [
-            //     'name' => 'your-api-name',
-            //     'url' => env('your-api-url'),
-            //     'params' => ['your-api-source-from-date-key' => $this->getLastCall('your-api-name'),'your-api-source-to-date-key' => Carbon::now()->toDateString()],
-            //     'headers' => ['api-key' => env('your-api_API_KEY')],
-            //     'start_page' => your-api-response-start-paging-number,
-            //     'total_key' => 'your-api-response-key-where-total-results-are-stored',
-            //     'page_size' => 'your-api-response-page-size',
-            //     'mapper' => new ExampleMapper()
-            // ],
         ];
+    }
 
+    private function fetchAndMapData($source)
+    {
+        $query = http_build_query($source['params']);
+        $url = "{$source['url']}?$query";
         
-        foreach ($sources as $source) {
-            $mapped_data = [];
-
-            $query = http_build_query($source['params']);
-            $url = "{$source['url']}?$query";
-            
-            $fetcher = new NewsFetcher($url, $source['headers'], $source['start_page'], $source['total_key'], $source['page_size']);
-            $data = $fetcher->getData();
-            $mapped_data[] = $source['mapper']->mapData($data);
-            $flattened_data = array_merge(...$mapped_data);
-            
-            if (empty($flattened_data)) {
-                Log::info('No new articles found in ' . $source['name']);
-                continue;
-            }
-
-            StoreArticles::dispatch($flattened_data, $source['name']);
-        }
+        $fetcher = new NewsFetcher($url, $source['headers'], $source['start_page'], $source['total_key'], $source['page_size']);
+        $data = $fetcher->getData();
+        $mapped_data[] = $source['mapper']->mapData($data);
+        
+        return array_merge(...$mapped_data);
     }
 
     private function getLastCall($sourceName)
